2b39788c9f2a199edc4accb51eb1d4690730cf3d
vaibhav sanmani <vaibhavsanmani9@gmail.com>
2026-04-10 21:58:36 +0530

    Updated product section

--- src/landing_page/about/Team.jsx
+++ src/landing_page/about/Team.jsx
@@ -1,7 +1,35 @@
 import React from 'react'
 function Team() {
     return ( 
-        <h1>Team Page</h1>
+         <div className="container">
+            <div className="row">
+                <h4 className=' mb-5 text-center'>People</h4>
+                <div className="col-1"></div>
+                <div className="col-5 text-center">
+                   <img src="media/images/nithinKamath.jpg" style={{width:"65%",borderRadius:"50%"}}/>
+                   <h4 className='mt-4'>Nitin Kamath</h4>
+                   <p className='text-muted mt-3'>Founder, CEO</p>
+                </div>
+                <div className="col-5 people" >
+                    <p >Nithin bootstrapped and founded Zerodha in 2010 to overcome the<br/> 
+                        hurdles he faced during his decade long stint as a trader. Today,<br/> 
+                        Zerodha has changed the landscape of the Indian broking industry. <br/>
+                    </p>
+
+                    <p>He is a member of the SEBI Secondary Market Advisory Committee<br/> 
+                        (SMAC) and the Market Data Advisory Committee (MDAC).<br/> 
+                    </p>
+                    <p>Playing basketball is his zen.<br/> </p>
+
+                    <p className='mb-5'>Connect on&nbsp; 
+                        <a href="/"  style={{textDecoration:"none"}}>Homepage</a> 
+                         / <a href="/"  style={{textDecoration:"none"}}>TradingQnA</a> 
+                         / <a href="/"  style={{textDecoration:"none"}}>Twitter</a><br/> </p>
+                    
+                </div>
+                <div className="col-1"></div>
+            </div>
+        </div>
      );
 }
 
--- src/landing_page/products/Hero.jsx
+++ src/landing_page/products/Hero.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
 function Hero() {
     return ( 
-        <h1>Hero Section</h1>
+        <>
+         <div className='container p-5 mt-5 mb-5'>
+                <div className="row text-center">
+                    <h3>Zerodha Products</h3>
+                    <p style={{fontSize:"20px"}}>Sleek, modern, and intuitive trading platforms</p>
+                    <p>Check Out Our <a href="/" style={{textDecoration:"none"}} >investing offerings&nbsp;
+                    <i class="fa-solid fa-arrow-right-long"></i> </a></p>
+                </div> 
+                
+                 
+            </div>
+
+        </>
      );
 }
 
