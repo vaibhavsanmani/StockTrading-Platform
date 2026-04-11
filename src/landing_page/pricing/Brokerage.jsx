f6922b2c14156f91c8e5353de58170506f74909f
vaibhav sanmani <vaibhavsanmani9@gmail.com>
2026-04-12 02:52:49 +0530

    setting up support page

--- src/landing_page/pricing/Brokerage.jsx
+++ src/landing_page/pricing/Brokerage.jsx
@@ -1,15 +1,22 @@
 import React from 'react'
 function Brokerage() {
     return ( 
-        <div className="container">
+        <div className="container mt-5 mb-4">
             <div className="row">
-                <div className="col-8">
-                    <ul>
-                        
+                <div className="col-8 mb-5 mt-5">
+                    <h4 className='text-center text-muted' style={{color:'lightblue'}}>Brokerage calculator</h4>
+                    <ul className='text-left text-muted 'style={{lineHeight:'30px'}} >
+                        <li>Call & Trade and RMS auto-squareoff: Additional charges of ₹50 + GST per order.</li>
+                        <li>Digital contract notes will be sent via e-mail.</li>
+                        <li>Physical copies of contract notes, if required, shall be charged ₹20 per contract note. Courier charges apply.</li>
+                        <li>For NRI account (non-PIS), 0.5% or ₹100 per executed order for equity (whichever is lower).</li>
+                        <li>For NRI account (PIS), 0.5% or ₹200 per executed order for equity (whichever is lower).</li>
+                        <li>If the account is in debit balance, any order placed will be charged ₹40 per executed order instead of ₹20 per executed order.</li>
+
                     </ul>
                 </div>
-                <div className="col-4">
-
+                <div className="col-4 mt-5 mb-5">
+                    <h4 className='text-muted'>List of charges</h4>
                 </div>
             </div>
         </div>
--- src/landing_page/support/CreateTicket.jsx
+++ src/landing_page/support/CreateTicket.jsx
@@ -1,8 +1,115 @@
-import React from 'react'
+import React from "react";
+
 function CreateTicket() {
-    return (  
-        <h1>Create Ticket</h1>
-    );
+  return (
+    <div
+      style={{
+        backgroundColor: "#f7f7f7",
+        padding: "50px 0",
+        minHeight: "100vh",
+      }}
+    >
+      <div className="container mb-5">
+
+        <div className="row">
+
+          {/* Column 1 */}
+          <div className="col-md-4 mb-4">
+            <h5>
+              <i className="fa fa-plus-circle me-2"></i>
+              Account Opening
+            </h5>
+
+            <div className="mt-3 d-flex flex-column gap-2">
+              <a href="#">Online Account Opening</a>
+              <a href="#">Offline Account Opening</a>
+              <a href="#">Company, Partnership and HUF Account Opening</a>
+              <a href="#">NRI Account Opening</a>
+              <a href="#">Charges at Zerodha</a>
+              <a href="#">Zerodha IDFC FIRST Bank 3-in-1 Account</a>
+              <a href="#">Getting Started</a>
+            </div>
+          </div>
+
+          {/* Column 2 */}
+          <div className="col-md-4 mb-4">
+            <h5>
+              <i className="fa fa-user me-2"></i>
+              Your Zerodha Account
+            </h5>
+
+            <div className="mt-3 d-flex flex-column gap-2">
+              <a href="#">Login Credentials</a>
+              <a href="#">Account Modification and Segment Addition</a>
+              <a href="#">DP ID and bank details</a>
+              <a href="#">Your Profile</a>
+              <a href="#">Transfer and conversion of shares</a>
+            </div>
+          </div>
+
+          {/* Column 3 */}
+          <div className="col-md-4 mb-4">
+            <h5>
+              <i className="fa fa-bar-chart me-2"></i>
+              Your Zerodha Account
+            </h5>
+
+            <div className="mt-3 d-flex flex-column gap-2">
+              <a href="#">Margin/leverage, Product and Order types</a>
+              <a href="#">Kite Web and Mobile</a>
+              <a href="#">Trading FAQs</a>
+              <a href="#">Corporate Actions</a>
+              <a href="#">Sentinel</a>
+              <a href="#">Kite API</a>
+              <a href="#">Pi and other platforms</a>
+              <a href="#">Stockreports+</a>
+              <a href="#">GTT</a>
+            </div>
+          </div>
+
+        </div>
+
+        {/* Second Row */}
+        <div className="row mt-5">
+
+          <div className="col-md-4">
+            <h5>
+              <i className="fa fa-credit-card me-2"></i>
+              Funds
+            </h5>
+            <div className="mt-3 d-flex flex-column gap-2">
+              <a href="#">Adding Funds</a>
+              <a href="#">Fund Withdrawal</a>
+            </div>
+          </div>
+
+          <div className="col-md-4">
+            <h5>
+              <i className="fa fa-refresh me-2"></i>
+              Console
+            </h5>
+            <div className="mt-3 d-flex flex-column gap-2">
+              <a href="#">Reports</a>
+              <a href="#">Ledger</a>
+            </div>
+          </div>
+
+          <div className="col-md-4">
+            <h5>
+              <i className="fa fa-circle-o me-2"></i>
+              Coin
+            </h5>
+            <div className="mt-3 d-flex flex-column gap-2">
+              <a href="#">Understanding Mutual Funds</a>
+              <a href="#">About Coin</a>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </div>
+  );
 }
 
 export default CreateTicket;
