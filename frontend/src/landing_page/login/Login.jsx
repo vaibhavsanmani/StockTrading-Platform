13782710c923e0b37ed0afdbfc7bf64101ad3af0
vaibhavsanmani <vaibhavsanmani9@gmail.com>
2026-06-17 22:48:07 +0530

    making login and signup page responsive

--- frontend/src/landing_page/login/Login.jsx
+++ frontend/src/landing_page/login/Login.jsx
@@ -11,7 +11,7 @@ const Login = () => {
   return (
     <div className="container py-5">
       <div className="row justify-content-center">
-        <div className="col-md-6">
+        <div className="col-12 col-sm-10 col-md-6">
           <div className="card shadow-sm">
             <div className="card-body">
               <h1 className="h4 mb-3">Login</h1>
